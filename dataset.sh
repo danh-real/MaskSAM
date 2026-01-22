71afdcbb7743713087021ab371d933007e036d40
danh-real <htduyanh210604@gmail.com>
2026-01-22 10:25:28 +0000

    masksam

--- dataset.sh
+++ dataset.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-export nnUNet_raw="/data/code/MaskSAM/DATASETS/nnUnet_raw"
-export nnUNet_preprocessed="/data/code/MaskSAM/DATASETS/nnUnet_preprocessed"
-export nnUNet_results="/data/code/MaskSAM/DATASETS/nnUnet_results"
+export nnUNet_raw="/data/rlsam2/MaskSAM/DATASETS/nnUnet_raw"
+export nnUNet_preprocessed="/data/rlsam2/MaskSAM/DATASETS/nnUnet_preprocessed"
+export nnUNet_results="/data/rlsam2/MaskSAM/DATASETS/nnUnet_results"
 
-ROOT=/data/code/MaskSAM/DATASETS/nnUnet_raw
+ROOT=/data/rlsam2/MaskSAM/DATASETS/nnUnet_raw
 
-# nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task01_BrainTumour -np 8
-# nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task02_Heart -np 8
-# nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task03_Liver -np 8
-# nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task04_Hippocampus -np 8
 nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task05_Prostate -np 8
 nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task06_Lung -np 8
 nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task07_Pancreas -np 8
 nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task08_HepaticVessel -np 8
 nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task09_Spleen -np 8
-# nnUNetv2_convert_MSD_dataset -i ${ROOT}/Task10_Colon -np 8
 
 
 # python nnUNet/nnunetv2/dataset_conversion/convert_sarcoma.py
--- plan.sh
+++ plan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export nnUNet_raw="/data/code/MaskSAM/DATASETS/nnUnet_raw"
-export nnUNet_preprocessed="/data/code/MaskSAM/DATASETS/nnUnet_preprocessed"
-export nnUNet_results="/data/code/MaskSAM/DATASETS/nnUnet_results"
+export nnUNet_raw="/data/rlsam2/MaskSAM/DATASETS/nnUnet_raw"
+export nnUNet_preprocessed="/data/rlsam2/MaskSAM/DATASETS/nnUnet_preprocessed"
+export nnUNet_results="/data/rlsam2/MaskSAM/DATASETS/nnUnet_results"
 
 # nnUNetv2_plan_and_preprocess -d 001 -c 3d_fullres --verify_dataset_integrity
 # nnUNetv2_plan_and_preprocess -d 002 -c 3d_fullres --verify_dataset_integrity
