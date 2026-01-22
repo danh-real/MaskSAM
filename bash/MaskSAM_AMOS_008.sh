4d4df4529bc9fd552060eb87ccb803b5b7c5f4d8
danh-real <htduyanh210604@gmail.com>
2026-01-22 10:10:06 +0000

    masksam

---
+++ bash/MaskSAM_AMOS_008.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export nnUNet_raw="/data/code/MaskSAM/DATASETS/nnUnet_raw"
+export nnUNet_preprocessed="/data/code/MaskSAM/DATASETS/nnUnet_preprocessed"
+export nnUNet_results="/data/code/MaskSAM/DATASETS/nnUnet_results"
+
+nnUNetv2_train 008 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
