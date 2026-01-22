543892c872ccb23424bd38e83e72d75ae301a62a
danh-real <htduyanh210604@gmail.com>
2026-01-22 10:09:58 +0000

    masksam

--- bash/MaskSAM_AMOS.sh
+++ bash/MaskSAM_AMOS.sh
@@ -4,10 +4,4 @@ export nnUNet_raw="/data/code/MaskSAM/DATASETS/nnUnet_raw"
 export nnUNet_preprocessed="/data/code/MaskSAM/DATASETS/nnUnet_preprocessed"
 export nnUNet_results="/data/code/MaskSAM/DATASETS/nnUnet_results"
 
-# CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 201 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
-# CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 001 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
-# CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 002 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
-# CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 003 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
-# CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 004 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
-# CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 010 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
 CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 005 3d_fullres all -tr MaskSAM_AMOS -p nnUNetPlans -num_gpus 1 --c
